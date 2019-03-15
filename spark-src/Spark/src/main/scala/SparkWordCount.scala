7b9704ba66df43753b7fb4adce7abc75dbbada62
Monica Gondolini <monica.gondolini@studio.unibo.it>
2019-03-15 13:32:40 +0100

    SparkWordCount implemented.

--- spark-src/Spark/src/main/scala/SparkWordCount.scala
+++
@@ -1,8 +0,0 @@
-import org.apache.spark.{SparkConf, SparkContext}
-
-object SparkWordCount {
-  def main(args: Array[String]): Unit ={
-    val sc = new SparkContext(new SparkConf().setAppName("Spark Count"))
-    System.out.println("")
-  }
-}
--- spark-src/Spark/src/main/scala/exercise1/EntryPoint.scala
+++ spark-src/Spark/src/main/scala/exercise1/EntryPoint.scala
@@ -1,5 +1,9 @@
+package exercise1
+
 import org.apache.spark.SparkContext
 
 object EntryPoint extends App {
   val sc = SparkContext.getOrCreate()
+
+  //spark2-submit nomefile.jar
 }
