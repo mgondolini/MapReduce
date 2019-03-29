cded044bec65da9f7e7c74199ca4c16d152c884b
Monica Gondolini <monica.gondolini@studio.unibo.it>
2019-03-29 13:49:14 +0100

    Implemented ex5 and ex6.

--- spark-src/Spark/src/main/scala/E8/Exercise6.scala
+++ spark-src/Spark/src/main/scala/E8/Exercise6.scala
@@ -1,16 +1,18 @@
 package E8
-import Setup.sc
-import org.apache.spark.streaming._
+
+import E8.Setup.sc
 import org.apache.spark.storage.StorageLevel
-object Exercise5 extends App{
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+
+object Exercise6 extends App{
 
   val ssc = new StreamingContext(sc, Seconds(10))
   val lines = ssc.socketTextStream("137.204.72.242",9999,StorageLevel.MEMORY_AND_DISK_SER).window(Seconds(60), Seconds(10))
   val columns = lines.filter(_.nonEmpty).map(_.split("\\|"))
-  val tweet = columns.map(x => x(2))
-  val hashtag = tweet.flatMap(_.split(", "))
-  val hashtagCount = hashtag.map(x=>(x,1)).reduceByKey(_ + _).map({case(k,v)=>(v,k)}).transform({ rdd => rdd.sortByKey(false) })
-  hashtagCount.print()
+  val city = columns.map(x => x(4))
+
+  val cityCount = city.map(x=>(x,1)).reduceByKey(_ + _).map({case(k,v)=>(v,k)}).transform({ rdd => rdd.sortByKey(false) })
+  cityCount.print()
 
   ssc.start()
 
