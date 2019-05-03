f5d0b8855ed6c9bd58faa5de13d461ab5fc586f3
Monica Gondolini - monica.gondolini@studio.unibo.it <monica.gondolini@studio.unibo.it>
2019-05-03 12:59:48 +0200

    completed exercise2.

--- spark-src/Spark/src/main/scala/E8/Exercise2.scala
+++ spark-src/Spark/src/main/scala/E8/Exercise2.scala
@@ -9,12 +9,21 @@ class Exercise2 {
   // SQL context
   val sqlContext = new org.apache.spark.sql.SQLContext(sc)
 
-
   val url = "jdbc:oracle:thin:amordenti/amordenti@137.204.78.85:1521/SISINF"
-  val table = "amordenti.CRM_POS_ATTRIBUTES"
+  val attributes = "amordenti.CRM_POS_ATTRIBUTES"
   val attributesDF = sqlContext.read.format("jdbc")
-    .options(Map("driver"->"oracle.jdbc.driver.OracleDriver","url" -> url,"dbtable" -> table))
+    .options(Map("driver"->"oracle.jdbc.driver.OracleDriver","url" -> url,"dbtable" -> attributes))
     .load()
 
+  attributesDF.show()
+
+  val potential = "amordenti.CRM_POS_POTENTIAL"
+  val potentialDF = sqlContext.read.format("jdbc")
+    .options(Map("driver"->"oracle.jdbc.driver.OracleDriver","url" -> url,"dbtable" -> potential))
+    .load()
+  potentialDF.show()
+
+  val PPE_DF = sqlContext.sql("select * from gruppo5fila2.ppec")
+  PPE_DF.show()
 
 }
